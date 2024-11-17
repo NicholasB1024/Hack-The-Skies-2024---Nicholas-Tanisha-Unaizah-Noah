df1f7d42ee9600dd7be8dd97575b6f8559511ae8
NoahV2048 <noahverdon2008@gmail.com>
2024-11-17 13:59:23 -0500

    started with flask

--- backend/Mapping.py
+++ backend/Mapping.py
@@ -11,6 +11,10 @@ class City:
         self.city, self.province, self.country = city, province, country
 
 locConstant = City('Toronto', 'Ontario', 'Canada') # constant information for locations
+print(f'Accessing graph of {locConstant.city} (takes a little too long as of now)...') # generate city graph
+graph = ox.graph_from_place(f'{locConstant.city, locConstant.province, locConstant.country}', network_type='drive')
+# full city can take a while, so radial from locStart can be used instead temporarily (below)
+# graph = ox.graph_from_point((locStart.latitude, locStart.longitude), dist=2000, network_type='drive')
 
 def address_validation(msg: str): # determine pickup and dropoff spots
     while True:
@@ -18,36 +22,34 @@ def address_validation(msg: str): # determine pickup and dropoff spots
         while not location:
             address = input(f'Enter a valid {msg} in {locConstant.city}: ').strip()
             location = locator.geocode(f'{address}, {locConstant.city}, {locConstant.province}, {locConstant.country}')
-        print(location.address)
         confirmation = None
         while confirmation not in ('y', 'n'):
-            confirmation = input('Confirm address:\n(y/n): ').lower()
+            print('Selected address:', location.address)
+            confirmation = input('Confirm address (y/n): ').lower()
         if confirmation == 'y':
             break
     return location
 
-# Pickup and dropoff
-locStart = address_validation('pickup address')
-locStop = address_validation('dropoff address')
+while True:
+    # Select pickup and dropoff
+    locStart = address_validation('pickup address')
+    locStop = address_validation('dropoff address')
 
-# Find shortest path
-print(f'Accessing graph of {locConstant.city} (takes a little too long as of now)...')
-# graph of toronto below, takes a while, so radial from locStart is used instead temporarily
-# graph = ox.graph_from_place(f'{locConstant.city, locConstant.province, locConstant.country}', network_type='drive')
-graph = ox.graph_from_point((locStart.latitude, locStart.longitude), dist=2000, network_type='drive')
-node1 = ox.distance.nearest_nodes(graph, float(locStart.longitude), float(locStart.latitude))
-node2 = ox.distance.nearest_nodes(graph, float(locStop.longitude), float(locStop.latitude))
+    # Find corresponding graph nodes for each location
+    node1 = ox.distance.nearest_nodes(graph, float(locStart.longitude), float(locStart.latitude))
+    node2 = ox.distance.nearest_nodes(graph, float(locStop.longitude), float(locStop.latitude))
 
-print('Finding shortest path...')
-path = ox.shortest_path(G=graph, orig=node1, dest=node2, cpus=None)
-print('\nShortest Path Length:', len(path))
+    # Find shortest path
+    print('\nFinding shortest path...')
+    path = ox.shortest_path(G=graph, orig=node1, dest=node2, cpus=None)
+    print('Shortest Path Length:', len(path))
 
-# Print directions
-print('\nDirections:')
-prevStreetName = None
-for i in range(len(path) - 1):
-    streetName = graph[path[i]][path[i + 1]][0].get('name')
-    if not (streetName == None or streetName == prevStreetName):
-        print(streetName)
-    prevStreetName = streetName
-print('Arrived!\n')
+    # Print directions
+    print('\nDirections:')
+    prevStreetName = None
+    for i in range(len(path) - 1):
+        streetName = graph[path[i]][path[i + 1]][0].get('name')
+        if not (streetName == None or streetName == prevStreetName):
+            print(streetName)
+        prevStreetName = streetName
+    print('Arrived!\n')
