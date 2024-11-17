cd6d229cd28c2617ae6f75d1faee2f5c18f85045
Tanisha Hossain <tanishahossain15@gmail.com>
2024-11-17 11:50:40 -0500

    attempted connection for route finding in locationinput

---
+++ frontend/src/components/Greeting.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const Greeting = ({ name }) => {
+    return <h2>Hello, {name}! Where would you like to go today?</h2>;
+};
+export default Greeting;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header style={{ textAlign: 'center', padding: '1rem', background: '#282c34', color: 'white' }}>
+            <h1>PATHFINDR</h1>
+            <h2>The New Way To Get Around</h2>
+        </header>
+    );
+};
+
+export default Header;
